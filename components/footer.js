97637ab48fa7f7c361d6c3d2dfb2d0f6685e5bd9
Rahul Gupta <rahul@Rahuls-MacBook-Pro.local>
2020-02-25 14:12:52 -0800

    cleanup

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer>
       <p>
-        <a href="https://twitter.com/rahulkartgupta">Twitter</a><
+        <a href="https://twitter.com/rahulkartgupta">Twitter</a>
         <a href="https://www.linkedin.com/in/rahulkartgupta/">Linkedin</a>
       /p>
       <style jsx>{`
