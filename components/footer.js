bf4e5b358e9521ad1aeb1dfac77bc1d67ac983e3
Rahul Gupta <rahul@Rahuls-MacBook-Pro.local>
2020-02-25 14:11:11 -0800

    cleanup

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,10 @@
 function Footer() {
   return (
     <footer>
-      <p><a href="https://twitter.com/rahulkartgupta">Twitter</a></p>
-      <p><a href="https://www.linkedin.com/in/rahulkartgupta/">Linkedin</a></p>
-      
+      <p>
+        <a href="https://twitter.com/rahulkartgupta">Twitter</a><
+        <a href="https://www.linkedin.com/in/rahulkartgupta/">Linkedin</a>
+      /p>
       <style jsx>{`
         footer {
           padding: 1em 0;
