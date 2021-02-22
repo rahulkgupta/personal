e493f2f85461c6a60e963620ec105a04a459b05b
Rahul Gupta <rahulkartgupta@gmail.com>
2021-02-22 15:04:44 -0800

    blah

--- posts/get-blog-posts.js
+++ posts/get-blog-posts.js
@@ -1,30 +1,31 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-const DIR = path.join(process.cwd(), '/pages/posts/')
-console.log(DIR)
-const META = /export\s+const\s+meta\s+=\s+({[\s\S]*?\n})/
+const DIR = path.join(process.cwd(), "/pages/posts/");
+console.log(DIR);
+const META = /export\s+const\s+meta\s+=\s+({[\s\S]*?\n})/;
 const files = fs
   .readdirSync(DIR)
-  .filter(file => file.endsWith('.md') || file.endsWith('.mdx'))
+  .filter((file) => file.endsWith(".md") || file.endsWith(".mdx"));
 
 module.exports = files
   .map((file, index) => {
-    const name = path.join(DIR, file)
-    const contents = fs.readFileSync(name, 'utf-8')
-    const match = META.exec(contents)
-    console.log(name)
-    if (!match || typeof match[1] !== 'string') {
-      throw new Error(`${name} needs to export const meta = {}`)
+    const name = path.join(DIR, file);
+    const contents = fs.readFileSync(name, "utf-8");
+    const match = META.exec(contents);
+    console.log("test");
+    console.log(name);
+    if (!match || typeof match[1] !== "string") {
+      throw new Error(`${name} needs to export const meta = {}`);
     }
 
     // eslint-disable-next-line no-eval
-    const meta = eval('(' + match[1] + ')')
+    const meta = eval("(" + match[1] + ")");
     return {
       ...meta,
-      path: '/posts/' + file.replace(/\.mdx?$/, ''),
+      path: "/posts/" + file.replace(/\.mdx?$/, ""),
       index,
-    }
+    };
   })
-  .filter(meta => meta.published)
-  .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
+  .filter((meta) => meta.published)
+  .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
